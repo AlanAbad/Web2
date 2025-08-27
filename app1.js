3af2dde3df4ad6d5170a8ed3efa69cf8e01e7684
AlanAbad <134542324+AlanAbad@users.noreply.github.com>
2025-08-26 23:03:13 -0600

    Add files via upload

---
+++ app1.js
@@ -0,0 +1,8 @@
+let variable1 = parseInt ( prompt("Ingresa primer número"));
+let variable2 = parseInt ( prompt("Ingresa segundo número"));
+
+console.log("La suma es: " + (variable1 + variable2));
+console.log("La resta es: " + (variable1 - variable2));
+console.log("La mult es: " + (variable1 * variable2));
+console.log("La div es: " + (variable1 / variable2));
+console.log("El mod  es: " + (variable1 % variable2));
